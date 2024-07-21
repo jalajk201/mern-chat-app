396773c9cba9b51cc7182fb8d9136d5d6c3c6ac7
Jalaj Singh <jsingh11_be21@thapar.edu>
2024-07-22 03:00:13 +0530

    4th part completed- socket io

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,9 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -15,21 +15,16 @@ dotenv.config();
 app.use(express.json()); //to parse the incoming requests with JSON payloads(from req.body)
 app.use(cookieParser()); // to parse cookies from incoming requests
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
-app.use("/api/users",userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // app.get("/", (req,res) => {
 //     // root route http://localhost:5000/
-//     res.send("Hello World!!") 
+//     res.send("Hello World!!")
 // });
 
-
-
-
-
-
-app.listen(PORT,() =>{
-    connectToMongoDB();
-    console.log(`Server running on port ${PORT}`);
+server.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server running on port ${PORT}`);
 });
--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import { IoSearchSharp } from "react-icons/io5";
+import useConversation from "../../zustand/useConversation";
+import { useState } from "react";
+
+import useGetConversations from "../../hooks/useGetConversations";
+import toast from "react-hot-toast";
 
 const SearchInput = () => {
+  const [search, setSearch] = useState("");
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversations();
+  const hadleSubmit = (e) => {
+    e.preventDefault();
+    // perform search here
+    if (!search) return;
+    if (search.length < 3) {
+      return toast.error("Search term must be atleast 3 characters long");
+    }
+    const conversation = conversations.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else toast.error("No such user found!");
+  };
+
   return (
-    <form className="flex items-center gap-2">
-      <input type="text" placeholder="Search..." className="input input-bordered rounded-full" />
+    <form onSubmit={hadleSubmit} className="flex items-center gap-2">
+      <input
+        type="text"
+        placeholder="Search..."
+        className="input input-bordered rounded-full"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <button type="submit" className="btn btn-circle bg-sky-500 text-white">
         <IoSearchSharp className="w-6 h-6 outline-none" />
       </button>
@@ -13,7 +43,6 @@ const SearchInput = () => {
 };
 
 export default SearchInput;
-
 
 // STARTER CODE FOR THIS FILE
 // import React from "react";
